c3d92465362fec5815757be88e5747967557807c
Rukovich <rukovich97@gmail.com>
2022-01-17 00:19:55 +0300

    index v.5

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let promo__menu = document.querySelector('.promo__menu');
+let Header_bars = document.querySelector('.burger');
+
+burger.addEventListener('click', function() {
+   promo__menu.classList.toggle ('hidden');
+});
+
+
+
